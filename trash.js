3ad6c5c1ddabed51d9908ece7b489f3c8854d860
Jake <jacobartz1987@gmail.com>
2019-12-03 21:34:17 -0700

    started switch

---
+++ trash.js
@@ -0,0 +1,36 @@
+
+
+
+
+// _querySelector = (event) => {
+//     let regExp;
+
+//     switch (this.state.switch) {
+
+//         case 1 :  
+//             regExp = new RegExp(this._regExpEscape(this.state.string), "ig")
+//             break
+//         case 2 :  
+//             regExp = new RegExp("\\b\\w*" + (this._regExpEscape(this.state.string)) + "\\w*\\b", "ig");
+//             break
+//         case 3 :  
+//             regExp = new RegExp('[^.?!]*(?<=[.?!\\s])\\b\\w*' + (this._regExpEscape(this.state.string)) + '\\w*\\b(?=[\\s.?!])[^.?!]*[.?!]', 'ig')
+//             break
+//         case 4 :  
+//             regExp = new RegExp('[^.?!]*(?<=[.?!\\s])(?=[\\s.?!])[^.?!]*[.?!]', 'ig')
+//             break
+//         default : break
+//         ;
+//     }
+//     console.log(regExp);
+//     let strArr = this.state.inputText.match(regExp);
+//     this.setState({ sentences: strArr })
+//     console.log(strArr);
+//     if (strArr !== null) {
+//         let count = strArr.length;
+//         this.setState({ ocurrences: count })
+//         console.log(count)
+//     } else {
+//         console.log("Yo, its null.")
+//     }
+// }
