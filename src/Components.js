6d79ef66a42e298ef55da6c2498e390a4f9a4dcb
Jake <jacobartz1987@gmail.com>
2019-12-07 22:33:37 -0700

    responsive css

--- src/Components.js
+++ src/Components.js
@@ -3,9 +3,10 @@ import TextOutput from "./Output";
 
 export default class Form extends React.Component {
     state = {
-        string: "th",
+        string: "",
         inputText: 'Hello user. \n\n This text area here is where you input the body of text you would like to parse, you may resize it by draggin the bottom left corner. The "Operative String" is the combination and sequence of characters that you want to look for. Here are some ideas: \n\n "From a literary analysis perspective, this would be extremely helpful in identifying patterns, verb use, tense, etc. For me, I would use this to search "ing" to help me identify potential gerund verbs that dont agree with my tense."\n\nMore queries will be added as well as persistent storage options, input options, and visualizations. Comments or suggestions are welcome.',
         ocurrences: 0,
+        wordCount: 0,
         words: [],
         sentences: [],
         totalSentences: 0
@@ -18,7 +19,6 @@ export default class Form extends React.Component {
     _countString = (event) => {
         event.preventDefault();
         let regExp = new RegExp(this._regExpEscape(this.state.string), "ig");
-        console.log(regExp);
         let strArr = this.state.inputText.match(regExp);
         console.log(strArr);
         if (strArr !== null) {
@@ -32,7 +32,6 @@ export default class Form extends React.Component {
     _wordsWithString = (event) => {
         event.preventDefault();
         let regExp = new RegExp("\\b\\w*" + (this._regExpEscape(this.state.string)) + "\\w*\\b", "ig");
-        console.log(regExp);
         let strArr = this.state.inputText.match(regExp);
         this.setState({ words: strArr })
         console.log(strArr);
@@ -47,7 +46,6 @@ export default class Form extends React.Component {
     _sentencesWithString = (event) => {
         event.preventDefault();
         let regExp = new RegExp('[^.?!]*(?<=[.?!\\s])\\b\\w*' + (this._regExpEscape(this.state.string)) + '\\w*\\b(?=[\\s.?!])[^.?!]*[.?!]', 'ig');
-        console.log(regExp);
         let strArr = this.state.inputText.match(regExp);
         this.setState({ sentences: strArr })
         console.log(strArr);
@@ -62,7 +60,6 @@ export default class Form extends React.Component {
     _totalSentences =(event)=> {
         event.preventDefault();
         let regExp = new RegExp('[^.?!]*(?<=[.?!\\s])(?=[\\s.?!])[^.?!]*[.?!]', 'ig');
-        console.log(regExp);
         let strArrA = this.state.inputText.match(regExp);
         let strArrB = [];
         for (let i=0;i<strArrA.length;i++) {
@@ -73,7 +70,7 @@ export default class Form extends React.Component {
         this.setState({ sentences: strArrB })
         if (strArrB !== null) {
             let count = strArrB.length;
-            this.setState({ ocurrences: count })
+            this.setState({ totalSentences: count })
             console.log(count)
         } else {
             console.log("Yo, its null.")
@@ -92,14 +89,15 @@ export default class Form extends React.Component {
                         />
                         <label htmlFor="msi" className="string-input-label">Operative String:</label>
                         <input type="text" name="msi" className="string-input"
+                        placeholder="enter a sequence of characters here, letters or numbers. This might be a word, a phrase, an amount, etc..."
                             value={this.state.string}
                             onChange={e => this.setState({ string: e.target.value })}
                         />
                     </div>
                     <div id="b">
-                        <h4>Methods:</h4>
+                        <h5>Methods:</h5>
                         <button name="count" onClick={this._countString}>Count the occurences of your string within the text.</button>
-                        <button name="words" onClick={this._wordsWithString}>Find all the words your string is used in, if your string is a word, this will return any longer words that have recently eaten it.</button>
+                        <button name="words" onClick={this._wordsWithString}>Find all the character groups your string is in, if your string is a full word, this will return any longer words that have recently eaten it.</button>
                         <button name="sentences" onClick={this._sentencesWithString}>This will return all the sentences that your string occurs in.</button>
                         <button name="tsentences" onClick={this._totalSentences}>Count total sentences without regard to Operative String.</button>
                     </div>
--- src/Output.js
+++ src/Output.js
@@ -4,11 +4,15 @@ import React from "react";
 export default function TextOutput(props) {
     return (
         <div>
-            <p>Ocurrences: {props.ocurrences} </p>
+            <ul>Counts:
+    <li>Total character groups: {props.wordCount}</li>
+                <li>Total Sentences: {props.totalSentences}</li>
+                <li>Ocurrences: {props.ocurrences} </li>
+            </ul>Words list:
             <ol name="word-list"id="word-list">
                 {props.words ? props.words.map((item, i) =>
                 React.createElement("li", { key: i }, props.words[i])) : "No list to show"}
-            </ol>
+            </ol>Sentences list:
             <ol name="sentence-list" id="sentence-list">
                 {props.sentences ? props.sentences.map((item, i) => 
                 React.createElement("li", { key: i }, props.sentences[i])) : "No list to show"}
